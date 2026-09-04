9d50564bbc6ab72479fb6aeb1458fd5f74ac8af9
fathnix <mhmd.fatih2106@gmail.com>
2026-09-04 14:53:46 +0700

    Route product

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\Product\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
+Route::apiResource('/products', ProductController::class);
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
