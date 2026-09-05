c315b359c244de9ba4f71287a60a83017dd21901
fathnix <mhmd.fatih2106@gmail.com>
2026-09-05 20:14:11 +0700

    comment route

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,17 @@ use App\Http\Controllers\Product\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+// authentikasi
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
-// api product
-Route::apiResource('/products', ProductController::class);
-Route::apiResource('/category', CategoryController::class);
 
+// authorisasi
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('/products', ProductController::class);
+    Route::apiResource('/category', CategoryController::class);
+});
 
+// bawaan dari laravel
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
